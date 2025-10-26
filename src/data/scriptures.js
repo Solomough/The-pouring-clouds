ea73094788b56a5186882f6259dce408a7c064c3
Solomon Moughkaa Zahemen <zahemenmoughkaa@gmail.com>
2025-10-26 21:34:55 +0100

    Create scriptures.js

---
+++ src/data/scriptures.js
@@ -0,0 +1,7 @@
+export const scriptures = [
+  "Ephesians 3:16 Christ’s strength within you",
+  "Daniel 12:3 Your light, light up your world ",
+  "1 Peter 2:9 You are chosen as an answer to many",
+  "Isaiah 60:1 Arise and shine, your light has come",
+  "Matthew 5:14 A city set on a hill cannot be hidden"
+]
