b0a54f996a1edd92cede263d79e0c6b51b0edfc6
Solomon Moughkaa Zahemen <zahemenmoughkaa@gmail.com>
2025-10-26 21:30:42 +0100

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Landing from './pages/Landing'
+import Timer from './pages/Timer'
+import Scripture from './pages/Scripture'
+import InsightForm from './pages/InsightForm'
+import Success from './pages/Success'
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/timer" element={<Timer />} />
+        <Route path="/scripture" element={<Scripture />} />
+        <Route path="/insight" element={<InsightForm />} />
+        <Route path="/success" element={<Success />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
